fa2349854c3c6bcf3d038ed26e4fbea6b1d5b570
Phạm Huy Thiên <55048442+PhamHuyThien@users.noreply.github.com>
2021-09-29 20:06:02 +0700

    [thienph] update login request (#2)
    * [master] update SERECT JwtTokenProvider
    
    * [masster] add swangui ./swagger-ui/
    
    * add entity to model folder and Override Account class
    
    * [thienph] finish model connection database mysql
    
    * [thienph] login update use db connect
    
    Co-authored-by: GDC\os_thienph <phamhuy.thien@etc.vn>
    Co-authored-by: vuongdam <vuong.dam@ntq-solution.com.vn>

--- src/main/java/com/datn/topfood/services/service/AuthServiceImpl.java
+++ src/main/java/com/datn/topfood/services/service/AuthServiceImpl.java
@@ -2,10 +2,12 @@ package com.datn.topfood.services.service;
 
 import com.datn.topfood.authen.JwtTokenProvider;
 import com.datn.topfood.data.model.Account;
+import com.datn.topfood.data.repository.jpa.AccountRepository;
 import com.datn.topfood.dto.request.LoginRequest;
 import com.datn.topfood.dto.response.LoginResponse;
 import com.datn.topfood.services.interf.AuthService;
 import com.datn.topfood.util.constant.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,21 +16,25 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+    @Autowired
+    AccountRepository accountRepository;
+
     @Override
     public LoginResponse loginWithUsername(LoginRequest loginRequest) {
-        //JPA sẽ tìm ra tk mk ở đây....
-        String username = "thiendeptrai";
-        String password = "p4ssw0rd";
-        if (!(loginRequest.getUsername().equals(username) && loginRequest.getPassword().equals(password))) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, Message.AUTH_USERNAME_OR_PASSWORD_WRONG);
+        UserDetails userDetails = loadUserByUsername(loginRequest.getUsername());
+        if(userDetails == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, Message.AUTH_LOGIN_USERNAME_WRONG);
         }
-        Account account = new Account(username, password);
-        String token = JwtTokenProvider.generateToken(account);
+        if(!userDetails.getPassword().equals(loginRequest.getPassword())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, Message.AUTH_LOGIN_PASSWORD_FAIL);
+        }
+        String token = JwtTokenProvider.generateToken(userDetails);
         return new LoginResponse(token);
     }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        Account account = accountRepository.findByUsername(s);
+        return account;
     }
 }
