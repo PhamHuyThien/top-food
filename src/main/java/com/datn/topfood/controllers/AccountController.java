2fc27c0a204e28bbfc7b112af7e46510c7c590c6
unknown <daokhai0108@gmail.com>
2021-10-04 19:28:09 +0700

    update

--- src/main/java/com/datn/topfood/controllers/AccountController.java
+++ src/main/java/com/datn/topfood/controllers/AccountController.java
@@ -1,14 +1,11 @@
 package com.datn.topfood.controllers;
 
 import com.datn.topfood.dto.request.*;
+import com.datn.topfood.dto.response.ProfileResponse;
+import com.datn.topfood.services.interf.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.datn.topfood.dto.response.FriendProfileResponse;
 import com.datn.topfood.dto.response.Response;
@@ -17,10 +14,14 @@ import com.datn.topfood.util.constant.Message;
 
 import io.swagger.v3.oas.annotations.Operation;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin
 @RequestMapping("/account")
 public class AccountController {
-
+    @Autowired
+    ProfileService profileService;
     @Autowired
     AccountService accountService;
 
@@ -43,5 +44,22 @@ public class AccountController {
     public ResponseEntity<Response<?>> active(@RequestBody ActiveRequest activeRequest) {
         accountService.active(activeRequest);
         return ResponseEntity.ok(new Response<>(true, Message.OTHER_SUCCESS));
+    }
+
+    @PostMapping(path = "")
+    public void createProfile(@RequestBody RegisterRequest create){
+        profileService.createProfile(create);
+    }
+    @PutMapping("/{id}")
+    public ProfileResponse updateProfile(@RequestBody ProfileRequest update, @PathVariable Long id){
+        return profileService.updateProfile(update,id);
+    }
+    @GetMapping("/{id}")
+    public ProfileResponse findById(@PathVariable Long id){
+        return profileService.findById(id);
+    }
+    @GetMapping(path = "")
+    public List<ProfileResponse> search(@RequestParam String name){
+        return profileService.SearchByNameAndPhone(name);
     }
 }
