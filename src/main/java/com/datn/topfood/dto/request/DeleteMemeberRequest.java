2d81dd77d63fa006c4608dbeef316ff03d657e33
PhamHuyThien <sync.mail.pc@gmail.com>
2021-10-10 22:39:30 +0700

    ok

---
+++ src/main/java/com/datn/topfood/dto/request/DeleteMemeberRequest.java
@@ -0,0 +1,9 @@
+package com.datn.topfood.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+public class DeleteMemeberRequest extends AddMemeberRequest {
+}
