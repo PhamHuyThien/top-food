d6fb8716f9aad627b7f29af20fbf0e711aee1f18
vuongdam11494 <damvuong0987@gmail.com>
2021-11-07 20:45:02 +0700

    change type food search request

--- src/main/java/com/datn/topfood/util/ConvertUtils.java
+++ src/main/java/com/datn/topfood/util/ConvertUtils.java
@@ -11,20 +11,20 @@ import com.datn.topfood.util.enums.FileType;
 
 public class ConvertUtils {
 
-	public static Set<File> convertArrFileReqToSetFile(List<FileRequest> arrFile){
+	public static Set<File> convertArrFileReqToSetFile(List<String> arrFile){
 		Set<File> files = new HashSet<>();
-		for(FileRequest f:arrFile) {
+		for(String f:arrFile) {
 			File file  = new File();
-			file.setPath(f.getPath());
-			file.setType(FileType.valueOf(f.getType().toUpperCase()));
+			file.setPath(f);
+			file.setType(FileType.DEFAULT);
 			files.add(file);
 		}
 		return files;
 	}
 	
-	public static List<FileRequest> convertSetToArrFile(Set<File> files){
+	public static List<String> convertSetToArrFile(Set<File> files){
 		return files.stream().map((f)->{
-			return new FileRequest(f.getPath(), f.getType().name);
+			return f.getPath();
 		}).collect(Collectors.toList());
 	}
 }
