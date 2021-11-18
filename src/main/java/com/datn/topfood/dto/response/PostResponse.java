2b16a4dcf37f0c3ea294d173e20c388ae21d81ef
vuongdam11494 <damvuong0987@gmail.com>
2021-11-18 18:58:34 +0700

    fix code

--- src/main/java/com/datn/topfood/dto/response/PostResponse.java
+++ src/main/java/com/datn/topfood/dto/response/PostResponse.java
@@ -23,4 +23,19 @@ public class PostResponse {
 	String[] address;
 	List<FoodResponse> foods;
 	ProfileResponse profile;
+	Long totalReaction;
+	boolean isMyReaction;
+	public PostResponse(Long id, String content, List<String> files, List<TagResponse> tags, String[] address,
+			List<FoodResponse> foods, ProfileResponse profile) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.files = files;
+		this.tags = tags;
+		this.address = address;
+		this.foods = foods;
+		this.profile = profile;
+	}
+	
+	
 }
